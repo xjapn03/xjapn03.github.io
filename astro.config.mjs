6fe31109809d2e62185c0920883a0c8ef1dd9a5c
Juan. <128620330+xjapn03@users.noreply.github.com>
2025-01-27 10:11:26 -0500

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://xjapn03.github.io',
     integrations: [
         mdx(),
         sitemap(),
